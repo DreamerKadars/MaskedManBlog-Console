52e38c9a9d9ab8f635caafa013fa081e844f6fc7
mashuo <3376566093@qq.com>
2022-05-02 02:09:08 +0800

    添加基本框架

--- src/Blog/MainPage.tsx
+++ src/Blog/MainPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export class MainPage extends React.Component<any,any> {
-    render() {
-        return (
-            <div>hello world</div>
-        )
-    }
+// export class MainPage extends React.Component<any,any> {
+//     render() {
+//         return (
+//             <div>hello world</div>
+//         )
+//     }
+// }
+
+const  MainPage:React.FC = () => {
+    return (<div>hello world</div>)
 }
+export default MainPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,34 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import { PageHeader, Message, Radio } from '@arco-design/web-react';
+import React, { useEffect, useState } from "react";
 import "@arco-design/web-react/dist/css/arco.css";
-import { Routes, Router, Route } from 'react-router'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import MainPage from "./Blog/MainPage"
 
-import { MainPage } from "./Blog/MainPage"
+const App2 = props => {
+  const [n, setN] = React.useState(0);  //useState()返回一个数组，第一个是读，第二个是写
+  const addN = () => {
+    setN(n + 1);
+  }
+  return (
+    <div>{n}
+      <button onclick={addN}>+1</button></div>
+  )
+}
 
-ReactDOM.render(
+function APP() {
+  let mashuo=React.createElement("h1", {title:"mashuo"},"hello react")
+  return <Router>
   <Routes>
-    <Route path="/" element={<MainPage />}>
-      <Route
-        path="messages"
-        element={<MainPage />}
-      />
-      <Route path="tasks" element={<MainPage />} />
-    </Route>
-    <Route path="about" element={<MainPage />} />
-  </Routes>,
-  document.getElementById('root')
-);
+      <Route path="/" element={mashuo} />
+      <Route path="/mashuo" element={<App2 />}/>
+     
+      {/* <Route path="/about" component={MainPage} />
+      <Route path="/topics" component={MainPage} /> */}
+    </Routes>
+  </Router>
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<APP />);
